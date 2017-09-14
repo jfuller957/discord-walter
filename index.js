4bdcdde40cb376305d4ad720c8f6fe7a24488634
James Fuller <james.fuller@cbs.com>
2017-09-14 10:03:47 -0700

    walter says hello

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+const Discord = require("discord.js");
+
+const TOKEN = "MzU2OTI5OTg3OTM0NDIxMDA0.DJigpg.AfnG_fnh6u4NPG6U5KaEh4qXDrE";
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+	console.log("Ready");
+});
+
+bot.on("message", function(message) {
+	if(message.author.equals(bot.user)) return;
+	if(message.content === "hello") {
+		message.channel.sendMessage("Hi there!");
+	}
+});
+
+bot.login(TOKEN);
