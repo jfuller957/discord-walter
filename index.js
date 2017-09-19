2f60c6e6df43817e9969b7695c934ef2191db3b1
James Fuller <james.fuller@cbs.com>
2017-09-19 13:52:12 -0700

    format changes

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
 
 // dependencies
-
 const Discord = require("discord.js");
 const credentials = require("./credentials.js");
 const yahooStocks = require("yahoo-stocks");
 
-//make new bot
-
+// make new bot
 const bot = new Discord.Client();
 
-//message to announce bot is functioning.
-
+// message to announce bot is connected to discord.
 bot.on("ready", function () {
 	console.log("I am ready!");
 });
 
-//make bot respond to the word hello
-
+// listen for message
 bot.on("message", function (message) {
+    // make bot respond to the word hello
 	if (message.content === "hello") {
 		message.reply("Well, hello there!");
 	}
 
-	//take the incoming message and make an array out of each word separating them at the spaces
+	// take the incoming message and make an array out of each word separating them at the spaces
 	let contents = message.content.split(" ");
 
-	//check for commands
-
-	//Help command
-	if (contents[0] === "!walter") {
-		message.reply("```css\n" + "Walter's abilities\n\n* !stock - Look up the current price of a stock.\n* !weather - Give current temp in given zip code.\n" + "\n```");
+	// check for commands
+	// Help command
+	if (contents[0] === "!commands") {
+		message.reply("```css\n" + "Walter's commands:\n\n* !stock - Look up the current price of a stock.\n* !weather - Give current temp in given zip code.\n" + "\n```");
 	};
 
-	//stock command
+	// stock command
 	if (contents[0] === "!stock") {
 		message.reply("Ok I will look up " + contents[1] + "!");
 
 		yahooStocks.lookup(contents[1]).then(function (response) {
-			message.reply("```js\n" + response.currentPrice + "\n```");
+			message.reply("```css\n" + response.currentPrice + "\n```");
 		});
 	}
 
@@ -45,7 +41,6 @@ bot.on("message", function (message) {
 
 
 
-//log bot in with its token
-
+// log bot in with its token
 bot.login(token);
 
