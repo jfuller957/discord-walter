a99a1a8559d7b156e116664facce555aa42df0b2
James Fuller <james.fuller@cbs.com>
2017-09-25 11:04:32 -0700

    added conversion command

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // dependencies
-const Discord     = require("discord.js");
-const credentials = require("./credentials.js");
-const yahooStocks = require("yahoo-stocks");
-const weather    = require("weather-js");
+const Discord       = require("discord.js");
+const credentials   = require("./credentials.js");
+const yahooStocks   = require("yahoo-stocks");
+const weather       = require("weather-js");
 
 // make new bot
 const bot = new Discord.Client();
@@ -24,7 +24,10 @@ bot.on("message", function (message) {
 
     // shows all of walters commands
     if (contents[0] === "!commands") {
-        message.reply("```css\n" + "Walter's commands:\n\n* !stock - Look up the current price of a stock.\n* !weather - Give current temp in given zip code.\n" + "\n```");
+        message.channel.send({embed: {
+            color: 3447003,
+            description: "Walter's commands:\n\n* !stock - Look up the current price of a stock.\n* !weather - Give current temp in given zip code.\n"
+        }});
     };
 
     // stock command
@@ -32,23 +35,67 @@ bot.on("message", function (message) {
         message.reply("Ok I will look up " + contents[1] + "!");
 
         yahooStocks.lookup(contents[1]).then(function (response) {
-            message.reply("```css\n" + response.currentPrice + "\n```");
-        });
+            message.channel.send({embed: {
+            color: 3447003,
+            description: JSON.stringify(response.currentPrice)
+        }});
+    });
     };
 
     // weather command
     if (contents[0] === "!weather") {
-        var weather = require('weather-js');
           
-        // search:     location name or zipcode 
-        // Options available are search by location name or zipcode and select degree type: F or C 
-        weather.find({search: contents.slice(1, 4).join(" "), degreeType: 'F'}, function(err, result) {
+        // options available are search by zipcode and select degree type: F or C 
+       weather.find({search: contents[1], degreeType: 'F'}, function(err, result) {
             if(err) console.log(err);
         
             message.reply(JSON.stringify(result, null, 2));
             console.log(JSON.stringify(result, null, 2));
         });
     };
+
+    // money command
+    if (contents[0] === "!convert") {
+
+        // place current rates in array
+        let rates = [0.83675, 1.23331, 0.81070, 0.67835, 1.19490, 1.47368];
+
+        // define conversion rates
+        let USDEUR = rates[0];
+        let USDCAD = rates[1];
+        let CADUSD = rates[2];
+        let CADEUR = rates[3];
+        let EURUSD = rates[4];
+        let EURCAD = rates[5];
+
+        // initialize result
+        let result = 0;
+
+        // conversion function
+        function convert (amount, convertFrom, convertTo) {
+            if (convertFrom === "USD" && convertTo === "EUR") {
+                result = amount * USDEUR;
+            } else if (convertFrom === "USD" && convertTo === "CAD") {
+                result = amount * USDCAD;
+            } else if (convertFrom === "CAD" && convertTo === "USD") {
+                result = amount * CADUSD;
+            } else if (convertFrom === "CAD" && convertTo === "EUR") {
+                result = amount * CADEUR;
+            } else if (convertFrom === "EUR" && convertTo === "USD") {
+                result = amount * EURUSD;
+            } else if (convertFrom === "EUR" && convertTo === "CAD") {
+                result = amount * EURCAD;
+            } else {
+                message.reply("Invalid input!");
+            }
+            console.log(result);
+            message.reply(result);
+        }
+        
+        convert(contents[1], contents[2], contents[3]);
+    };
+
+
 
 });
 
